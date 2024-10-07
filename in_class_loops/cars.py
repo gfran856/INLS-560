d4644535652bbf0f1edaa58f70dfe827dd1f4e1f
Grey.Francisco <gfran856@unc.edu>
2024-10-07 18:08:53 -0400

    Add in class loop

---
+++ in_class_loops/cars.py
@@ -0,0 +1,8 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.capitalize())
+# This is considered a '4' loop (above)
