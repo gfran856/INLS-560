e9634d9b672c4dc47b58fb4fcbebf14815a80e39
gfran856 <gfran856@unc.edu>
2024-10-24 09:37:02 -0400

    Updadated_cars.py
    new changes after canvas suggestion to make it work correctly

--- in_class_loops/cars.py
+++ in_class_loops/cars.py
@@ -1,7 +1,7 @@
 items = ['Cashews', 'Peanuts', 'Pecans', 'Brazilian Nuts']
 
 for item in items:
-    if item == "item3":
+    if item == item[3]:
         print(item.title()) 
     else:
         print(item.upper()) 
